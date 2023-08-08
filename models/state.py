5c2f68572b117a3937907da471ee6a720e1241ff
Santiago E <santy.e@gmail.com>
2023-08-07 19:30:41 -0600

    Upgrade: Task 6, States and Cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state', cascade="all, delete")
+
+    if HBNB_TYPE_STORAGE != "db":
+        @property
+        def cities(self):
+            """Return a list with the cities"""
+            from models import storage
+            city_dict = storage.all(City)
+            city_list = []
+            for value in city_dict.values():
+                if value.state_id == self.id:
+                    city_list.append(value)
+            return city_list
