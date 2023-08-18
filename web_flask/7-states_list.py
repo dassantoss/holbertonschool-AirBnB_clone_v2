82ecad1a628bf45da3e28b3386d4baf4e0de78aa
Santiago E <santy.e@gmail.com>
2023-08-17 20:22:56 -0600

    Add Flask route to display a list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application.
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """
+    Display a HTML page with a list of all State objects present
+    in DBStorage
+    """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_db():
+    """
+    Close the current SQLAlchemy session after each request
+    """
+    storage.close()
+
+
+if __name__ == ('_main__'):
+    app.run('0.0.0.0', port=5000)
