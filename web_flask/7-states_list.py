eb445ccd53f52b542354559bddde230e9c83468e
Santiago E <santy.e@gmail.com>
2023-08-19 14:20:44 -0600

    Fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application.
-"""
+"""Flask web application that returns an alternating html page"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    """
-    Display a HTML page with a list of all State objects present
-    in DBStorage
-    """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+@app.route('/states_list', strict_slashes=False)
+def states():
+    sl = storage.all(State)
+    return render_template('7-states_list.html', sl=sl)
 
 
 @app.teardown_appcontext
-def teardown_db():
-    """
-    Close the current SQLAlchemy session after each request
-    """
+def closing_time(self):
     storage.close()
 
-
-if __name__ == ('_main__'):
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
